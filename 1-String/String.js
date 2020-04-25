93a1fab6097fa3642b2ca9072d0460198dcf3a40
aamirali <sufaamir@gmail.com>
2020-04-25 13:48:09 -0400

    update

---
+++ 1-String/String.js
@@ -0,0 +1,19 @@
+//String
+
+// console.log('Aamir\n Ali')
+
+// String Primitive
+
+// var message = 'This is my\n first Message';
+
+var name1 = 'Aamir';
+
+var another =
+    `Hi ${name1},
+
+Thanks you for joining my mailing list ${3+2}.
+
+Regards,
+Aamir`
+
+console.log(another);
